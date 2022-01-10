9271815111174fd939214bc45300f8abe265d12f
Rick Martin <rmartin1985@att.net>
2022-01-09 21:05:18 -0600

    Refactored ProductItem

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from '../../utils/GlobalState';
+// removing and replacing with Redux
+// import { useStoreContext } from '../../utils/GlobalState';
+import { useSelector, useDispatch } from 'react-redux';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  });
+
+  const dispatch = useDispatch();
+
+  // Refactored for Redux
+  // const [state, dispatch] = useStoreContext();
 
   const { cart } = state;
 
