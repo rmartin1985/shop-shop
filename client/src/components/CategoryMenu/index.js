9b29c7817be9cef18e0c84ecdcde77a51e2c9fda
Rick Martin <rmartin1985@att.net>
2022-01-09 20:59:54 -0600

    Refactored CategoryMenu

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -2,11 +2,20 @@ import React, { useEffect } from 'react';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
-import { useStoreContext } from '../../utils/GlobalState';
+// replacing with Redux
+// import { useStoreContext } from '../../utils/GlobalState';
+import { useSelector, useDispatch } from 'react-redux';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state 
+  });
+  
+  const dispatch = useDispatch();
+
+  // refactored for Redux above
+  // const [state, dispatch] = useStoreContext();
 
   const { categories } = state;
 
